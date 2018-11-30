0025c65e1984b5e14d0aed9d297e9d7cbd04043d
Michael Gatewood <michael@mngatewood.com>
2018-11-30 11:54:39 -0700

    WIP

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,54 @@
-#code goes here
+class Story
+
+  attr_reader :section, 
+              :subsection, 
+              :title, 
+              :abstract, 
+              :link, 
+              :published, 
+              :photo,
+              :results,
+              :create_stories
+
+  def initialize(section, 
+                 subsection, 
+                 title, 
+                 abstract, 
+                 link, 
+                 published, 
+                 photo)
+    @section = section
+    @subsection = subsection
+    @title = title
+    @abstract = abstract
+    @link = link
+    @published = published
+    @photo = photo
+  end
+
+  def self.results
+    file = File.read('./data/nytimes.json')
+    hash = JSON.parse(file)
+    return hash["results"]
+  end
+
+  def self.create_stories
+    results = self.results
+    stories = []
+    results.each do |result|
+      section = result["section"]
+      subsection = result["section"]
+      title = result["title"]
+      abstract = result["abstract"]
+      link = result["url"]
+      published = result["published_date"]
+      photo_hash = result["multimedia"].find{|i|i["format"] == "Normal"}
+      photo = photo_hash ? photo_hash["url"] : "No Photo Available"
+      
+      story = Story.new(section, subsection, title, abstract, link, published, photo)
+      stories << story
+    end
+    return stories
+  end
+
+end
