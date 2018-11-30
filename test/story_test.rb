46d7e18d407ab189921a996055c8ecbc4fb117a9
Jordan Whitten <jordanwa1947@gmail.com>
2018-11-30 10:59:16 -0700

    refactored tests

--- test/story_test.rb
+++ test/story_test.rb
@@ -6,6 +6,12 @@ require './lib/story'
 class NytimesTest < Minitest::Test
 
   def test_it_exists
+    story = Story.new({})
+
+    assert_equal Story, story.class
+  end
+
+  def test_it_has_attributes
     story = Story.new(
       {section: "U.S.",
     subsection: "Politics",
@@ -16,13 +22,12 @@ class NytimesTest < Minitest::Test
     photo: "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg",}
     )
 
-    assert_equal Story, story.class
     assert_equal "U.S.", story.section
     assert_equal "Politics", story.subsection
     assert_equal "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal", story.title
     assert_equal "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.", story.abstract
     assert_equal "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html", story.link
     assert_equal "2018-11-29T18:46:27-05:00", story.published
-    assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg", story.photo 
+    assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg", story.photo
   end
 end
