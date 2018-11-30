5f0833f7e05bd783f345f4e82405f15824deb90d
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-11-30 12:02:57 -0700

    it's noon

--- lib/story.rb
+++ lib/story.rb
@@ -1,8 +1,16 @@
 require 'json'
 require 'pry'
 class Story
-  def initialize
+  attr_reader :section, :subsection, :title, :abstract
+
+  def initialize(index, file_path)
     file = File.read('data/nytimes.json')
     data_hash = JSON.parse(file)
+    stories = data_hash["results"][index]
+
+    @section = stories["section"]
+    @subsection = stories["subsection"]
+    @title = stories["title"]
+    @abstract = stories["abstract"]
   end
 end
--- test/story_test.rb
+++ test/story_test.rb
@@ -3,16 +3,24 @@ require 'minitest/pride'
 require 'pry'
 require './lib/story'
 
-
 class StoryTest < Minitest::Test
   def test_it_exists
-    story = Story.new
-    binding.pry
+    story = Story.new(0, './data/nytimes.json')
     assert_instance_of Story, story
   end
 
   def test_json_data_loads
-    @file = File.read('data/nytimes.json')
-    @data_hash = JSON.parse(file)
+    file = File.read('data/nytimes.json')
+    data_hash = JSON.parse(file)
+
+    assert_instance_of Hash, data_hash
+  end
+
+  def test_it_has_attributes
+    story = Story.new(0, './data/nytimes.json')
+    assert_equal 'U.S.', story.section
+    assert_equal 'Politics', story.subsection
+    assert_equal "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal", story.title
+    assert_equal "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.", story.abstract
   end
 end
