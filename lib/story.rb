0d698b6ead6c2a8ad535add76ee11d5671d859f0
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-11-30 10:56:03 -0700

    create story class

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,21 @@
-#code goes here
+# require './data/nytimes.json'
+
+class Story
+  attr_reader :section,
+              :subsection,
+              :title,
+              :abstract,
+              :link,
+              :published_date,
+              :photo
+
+  def initialize(section, subsection, title, abstract, link, published_date, photo)
+    @section = section
+    @subsection = subsection
+    @title = title
+    @abstract = abstract
+    @link = link
+    @published_date = published_date
+    @photo = photo
+  end
+end
---
+++ test/story_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/story'
+
+class StoryTest < Minitest::Test
+
+  def test_it_exists
+    story = Story.new("U.S.",
+                      "Politics",
+                      "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal",
+                      "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.",
+                      "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html",
+                      "2018-11-29T18:46:27-05:00",
+                      "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg"
+                      )
+    assert_instance_of Story, story
+  end
+
+  def test_it_has_attributes
+    story = Story.new("U.S.",
+                      "Politics",
+                      "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal",
+                      "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.",
+                      "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html",
+                      "2018-11-29T18:46:27-05:00",
+                      "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg"
+                      )
+
+    assert_equal "U.S.", story.section
+    assert_equal "Politics", story.subsection
+    assert_equal "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal", story.title
+    assert_equal "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.", story.abstract
+    assert_equal "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html", story.link
+    assert_equal "2018-11-29T18:46:27-05:00", story.published_date
+    assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg", story.photo
+  end
+
+end
