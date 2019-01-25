c6e7e313b17647802ca058e29f5592057765169b
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 09:58:50 -0700

    Adds story object tests.

--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -1,8 +1,8 @@
 require './lib/story.rb'
 
 describe Story do
-  it 'exists' do
-    data = {
+  before :each do
+    @data = {
       section: "one",
       subsection: "two",
       title: "three",
@@ -11,8 +11,31 @@ describe Story do
       published: "today's date",
       photo: "alink"
     }
-    story = Story.new(data)
+    @story = Story.new(@data)
+  end
 
-    expect(story).to be_an_instance_of Story
+  it 'exists' do
+    expect(@story).to be_an_instance_of Story
+  end
+  it 'has a section' do
+    expect(@story.section).to eq("one")
+  end
+  it 'has a subsection' do
+    expect(@story.subsection).to eq("two")
+  end
+  it 'has a title' do
+    expect(@story.title).to eq("three")
+  end
+  it 'has a abstract' do
+    expect(@story.abstract).to eq("four")
+  end
+  it 'has a link' do
+    expect(@story.link).to eq("alink")
+  end
+  it 'has a published' do
+    expect(@story.published).to eq("today's date")
+  end
+  it 'has a photo' do
+    expect(@story.photo).to eq("alink")
   end
 end
