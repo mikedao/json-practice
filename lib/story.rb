36acba8853190599cd782c73912d1655d92d8789
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-25 10:20:45 -0700

    basic setup working

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,30 @@
-#code goes here
+require 'json'
+
+class Story
+
+  attr_reader :section,
+              :subsection,
+              :title,
+              :abstract,
+              :link,
+              :published,
+              :photo
+
+  def initialize(hash)
+    @section = hash[:section]
+    @subsection = hash[:subsection]
+    @title = hash[:title]
+    @abstract = hash[:abstract]
+    @link = hash[:url]
+    @published = hash[:published_date]
+    @photo = hash[:photo]
+  end
+
+
+  def self.load
+    file = File.read('./data/nytimes.json')
+
+  end
+
+
+end
---
+++ test/story_test.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require 'minitest/pride'
+require 'pry'
+require './lib/story.rb'
+require 'json'
+
+class StoryTest < Minitest::Test
+  def test_it_has_attributes
+    article = {section: "U.S.",
+               subsection: "Politics",
+               title: "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal",
+               abstract: "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.",
+               url: "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html",
+               published_date: "2018-11-29T18:46:27-05:00",
+               photo: "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-thumbStandard.jpg"
+                      }
+
+    story = Story.new(article)
+    assert_instance_of Story, story
+    assert_equal "U.S.", story.section
+    assert_equal "Politics", story.subsection
+    assert_equal "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal", story.title
+    assert_equal "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.", story.abstract
+    assert_equal "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html", story.link
+    assert_equal "2018-11-29T18:46:27-05:00", story.published
+    assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-thumbStandard.jpg", story.photo
+  end
+end
