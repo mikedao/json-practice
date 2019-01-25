b40a488dc4f6e3dbdb20ade7126ab12af9ec60b5
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 09:14:39 -0700

    Adds storyloader class and adds and expectation to the first test.

--- spec/story_loader_spec.rb
+++ spec/story_loader_spec.rb
@@ -1,5 +1,7 @@
+require './lib/storyloader.rb'
 describe StoryLoader do
   it 'loads the stories into POROs' do
     stories = StoryLoader.new("nytimes.json")
+    expect(stories.length).to eq(43)
   end
 end
