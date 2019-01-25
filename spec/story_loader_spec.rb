c1798f8f93f1bed8f313b304feca6d61e5c8af76
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 09:19:13 -0700

    Adds to first test for story loader.

--- spec/story_loader_spec.rb
+++ spec/story_loader_spec.rb
@@ -2,6 +2,7 @@ require './lib/storyloader.rb'
 describe StoryLoader do
   it 'loads the stories into POROs' do
     stories = StoryLoader.new("nytimes.json")
-    expect(stories.length).to eq(43)
+    news = stories.load
+    expect(news.length).to eq(43)
   end
 end
