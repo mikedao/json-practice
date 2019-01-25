cf3377ab10c94d8a5a6c9663f2bd01e7109b0324
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-25 12:13:23 -0700

    initial commit

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,29 @@
-#code goes here
+require 'json'
+
+class Story
+  attr_reader :section, :subsection, :title,
+              :abstract, :link, :published,
+              :photo
+
+  def initialize(data = {})
+    @section = data[:section]
+    @subsection = data[:subsection]
+    @title = data[:title]
+    @abstract = data[:abstract]
+    @link = data[:url]
+    @published = data[:published_date]
+    @photo = data[:multimedia]
+  end
+
+  def self.load_json
+    file = File.read('./data/nytimes.json')
+    stories = JSON.parse(file, symbolize_names: true)
+    all = stories[:results].each do |story|
+      hash = {section: story[:section], subsection: story[:subsection],
+      title: story[:title], abstract: story[:abstract], link: story[:url],
+      published: story[:published_date], photo: story[:multimedia].first}
+      Story.new(hash)
+    end
+    all
+  end
+end
