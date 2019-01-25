e2cc5d10eac9ae60672706510603b914fc30b52e
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-25 12:37:30 -0700

    Makes final commit. Tests passing. Finalizes test it exists.

--- test/story_test.rb
+++ test/story_test.rb
@@ -5,11 +5,8 @@ require './lib/story'
 class StoryTest < Minitest::Test
 
   def test_it_exists
-    file = File.read('./data/nytimes.json')
-    json = JSON.parse(file)
-    results = json["results"]
+    story = Story.create_stories[0]
 
-    story = Story.new(results)
     assert_instance_of Story, story
   end
 
@@ -27,6 +24,5 @@ class StoryTest < Minitest::Test
     assert_equal story.published, "2018-11-29T18:46:27-05:00"
     assert_equal story.photo, "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-articleInline.jpg"
   end
-
 
 end
