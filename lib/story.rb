dd80bc969accfc71c5e60a7f608029de2605a267
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-25 11:44:43 -0700

    got array of story objects, need to test the if else method but it should be working

--- lib/story.rb
+++ lib/story.rb
@@ -15,16 +15,38 @@ class Story
     @subsection = hash[:subsection]
     @title = hash[:title]
     @abstract = hash[:abstract]
-    @link = hash[:url]
-    @published = hash[:published_date]
+    @link = hash[:link]
+    @published = hash[:published]
     @photo = hash[:photo]
   end
 
 
-  def self.load
+  def self.load_content
     file = File.read('./data/nytimes.json')
-
+    parsed = JSON.parse(file, symbolize_names: true)[:results]
+    story_data = parsed.map do |story|
+      if story[:multimedia][2][:format] == "Normal"
+        a = { section: story[:section],
+          subsection: story[:subsection],
+          title: story[:title],
+          abstract: story[:abstract],
+          link: story[:url],
+          published: story[:published_date],
+          photo: story[:multimedia][2][:url]
+          }
+      else
+        { section: story[:section],
+          subsection: story[:subsection],
+          title: story[:title],
+          abstract: story[:abstract],
+          link: story[:url],
+          published: story[:published_date],
+          photo: "No Photo Avaliable, Sorry!"
+          }
+      end
+    end
+    array_o_stories = story_data.map do |data|
+      Story.new(data)
+    end
   end
-
-
 end
--- test/story_test.rb
+++ test/story_test.rb
@@ -10,10 +10,10 @@ class StoryTest < Minitest::Test
                subsection: "Politics",
                title: "How a Lawyer, a Felon and a Russian General Chased a Moscow Trump Tower Deal",
                abstract: "During the presidential campaign, Michael D. Cohen and Felix Sater, an associate with a criminal past, pursued a new Trump Tower project with a former spymaster’s help.",
-               url: "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html",
-               published_date: "2018-11-29T18:46:27-05:00",
+               link: "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html",
+               published: "2018-11-29T18:46:27-05:00",
                photo: "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-thumbStandard.jpg"
-                      }
+            }
 
     story = Story.new(article)
     assert_instance_of Story, story
@@ -24,5 +24,10 @@ class StoryTest < Minitest::Test
     assert_equal "https://www.nytimes.com/2018/11/29/us/politics/trump-russia-felix-sater-michael-cohen.html", story.link
     assert_equal "2018-11-29T18:46:27-05:00", story.published
     assert_equal "https://static01.nyt.com/images/2018/11/30/world/30trumpmoscow-1-print/30trumpmoscow7-thumbStandard.jpg", story.photo
+  end
+
+  def test_it_can_create_articles
+    all_the_stories = Story.load_content
+    assert_equal(all_the_stories.count, 43)
   end
 end
