24001cd490e2b7a7949146953772a2a2b2b5ae45
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 10:22:04 -0700

    Adds code to pass storyloader test.

--- lib/storyloader.rb
+++ lib/storyloader.rb
@@ -1,3 +1,21 @@
-class StoryLoader
+require 'json'
+require 'story.rb'
 
-end 
+class StoryLoader
+  attr_reader :stories
+
+  def initialize(file)
+    @data = file
+  end
+
+  def load_stories
+    file = File.read(@data)
+    story_data = JSON.parse(file)["results"]
+    collection = []
+    story_data.each do |hash|
+      a_story = Story.new(hash)
+      collection << a_story
+    end
+    stories = collection
+  end
+end
