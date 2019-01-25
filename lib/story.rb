2a210e1cd17feccbd9b24929c66eabe83731606e
Brad Niedt <bradleyniedt@gmail.com>
2019-01-25 10:21:35 -0700

    Adds code to pass story spec.

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,18 @@
-#code goes here
+class Story
+  attr_reader :section,
+              :subsection,
+              :title,
+              :abstract,
+              :link,
+              :published,
+              :photo
+  def initialize(data)
+    @section = data["section"]
+    @subsection = data["subsection"]
+    @title = data["title"]
+    @abstract = data["abstract"]
+    @link = data["link"]
+    @published = data["published"]
+    @photo = data["multimedia"].first["url"]
+  end
+end
